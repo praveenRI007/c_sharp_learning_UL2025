3ba6c1ebc72eb4cc642170c0633f93b9e712efeb
Praveen_RI <Praveen_RI@LAPTOP-63KTA52S>
2025-10-14 12:47:56 +0100

    Add project files.

---
+++ learning/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using c_sharp_learning; // Namespace from your class library
+
+class Program
+{
+    static void Main()
+    {
+        Class1 myClass = new Class1();
+        Console.WriteLine("Testing library!");
+    }
+}
